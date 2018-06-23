a7935736cc4b27d6d445004197982a13e1eb9d34
pavankumargep <40364172+pavankumargep@users.noreply.github.com>
2018-06-23 13:38:13 +0530

    Commited

---
+++ Practice Git/Practice Git/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice_Git.Startup))]
+namespace Practice_Git
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

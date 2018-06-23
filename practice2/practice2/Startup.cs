913f76d194edc5eebeb3a199b3fac379ba5c693a
pavankumargep <40364172+pavankumargep@users.noreply.github.com>
2018-06-23 13:51:29 +0530

    Pused

---
+++ practice2/practice2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practice2.Startup))]
+namespace practice2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
